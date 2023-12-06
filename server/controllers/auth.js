0e16968acfca9e3c78fdc69a777be5f55cd474fb
suthyscott <sutherlandscott21@gmail.com>
2023-12-06 09:01:02 -0700

    register function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,7 @@
 const { SECRET } = process.env
 const jwt = require("jsonwebtoken")
+const { User } = require("../models/user")
+const bcrypt = require('bcryptjs')
 
 let createToken = (username, id) => {
     const token = jwt.sign({ username, id }, SECRET, { expiresIn: "2 days" })
@@ -9,12 +11,33 @@ let createToken = (username, id) => {
 module.exports = {
     register: async (req, res) => {
         try {
-            const { username, password} = req.body
-            // use the username and password to create a user in the db, and then that user's primary key is the id we use to create a token
-            const userToken = createToken(username, id)
-            res.status(200).send(userToken)
+            const { username, password } = req.body
+            let foundUser = await User.findOne({
+                where: { username: username.trim() }
+            })
+
+            if(foundUser){
+                res.status(400).send('That username has already been taken.')
+            } else {
+                const salt = bcrypt.genSaltSync(10)
+                const hash = bcrypt.hashSync(password, salt)
+
+                const newUser = await User.create({username, hashedPass: hash})
+
+                const token = createToken(newUser.username, newUser.id)
+
+                const exp = Date.now() + 1000 * 60 * 60 * 48
+
+                res.status(200).send({
+                    username: newUser.username,
+                    userId: newUser.id,
+                    token,
+                    exp
+                })
+            }
         } catch (err) {
             console.log(err)
+            res.sendStatus(500)
         }
     },
     login: async (req, res) => {
